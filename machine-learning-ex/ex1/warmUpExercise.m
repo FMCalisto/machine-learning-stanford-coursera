4cb53075426daab346925a070c8762e8930060ba
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2020-05-03 16:18:55 +0100

    [ADD] PDF Ex 1

--- machine-learning-ex/ex1/warmUpExercise.m
+++ machine-learning-ex/ex1/warmUpExercise.m
@@ -1,6 +1,7 @@
 function A = warmUpExercise()
 %WARMUPEXERCISE Example function in octave
-%   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+%   A = WARMUPEXERCISE() is an example
+% function that returns the 5x5 identity matrix
 
 A = [];
 % ============= YOUR CODE HERE ==============
