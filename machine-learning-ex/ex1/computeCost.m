e81cfa00f23080c5de815dc30900850bd7007f64
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2020-05-03 19:57:04 +0100

    [ADD] Computing Cost

--- machine-learning-ex/ex1/computeCost.m
+++ machine-learning-ex/ex1/computeCost.m
@@ -13,7 +13,22 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+%The vector of hypothesis
+hypo = X * theta;
 
+%Subtract the true y from the predicted y
+diff = hypo - y;
+
+%Square all the residuals
+sqResid = diff.^2;
+
+%Sum all the squared residuals
+sqSum = sum(sqResid);
+
+%return the resulting sum multiplied by (1/2m)
+J = (1 / (2 * m)) * sqSum;
+
+J
 
 
 
