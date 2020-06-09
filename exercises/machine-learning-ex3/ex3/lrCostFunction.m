7b17bfa6971b48cdb7fdaad684980cb4df4660d4
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2020-06-09 21:30:25 +0100

    [CREATE] Cost Function Vectorization

--- exercises/machine-learning-ex3/ex3/lrCostFunction.m
+++ exercises/machine-learning-ex3/ex3/lrCostFunction.m
@@ -25,25 +25,36 @@ grad = zeros(size(theta));
 %
 %       Each row of the resulting matrix will contain the value of the
 %       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
+%       the cost function and gradient computations.
 %
 % Hint: When computing the gradient of the regularized cost function, 
 %       there're many possible vectorized solutions, but one solution
 %       looks like:
 %           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           temp = theta;
+%           temp(1) = 0;   % because we don't add anything for j = 0
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+preds =  sigmoid(X*theta);
 
+leftPart = -y' * log(preds);
 
+rightPart = (1 - y') * log(1 - preds); 
 
+thetaZero = theta;
 
+thetaZero(1) = 0;
 
+lambaCostPart = (lambda / (2 * m)) * sum(thetaZero .^ 2);
 
+lambdaGradPart = lambda / m * thetaZero;
 
+J = (1 / m) * (leftPart - rightPart) + lambaCostPart;
 
+grad = ((1/m) * (X' * (preds - y)));
+
+grad = grad + lambdaGradPart;
 
 % =============================================================
 
