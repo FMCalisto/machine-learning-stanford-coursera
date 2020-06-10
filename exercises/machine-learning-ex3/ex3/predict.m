0ef33197bd99b7e04045fccebd26eb993f736589
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2020-06-10 23:54:12 +0100

    [CREATE] Predict

--- exercises/machine-learning-ex3/ex3/predict.m
+++ exercises/machine-learning-ex3/ex3/predict.m
@@ -21,13 +21,19 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(m, 1) X];
 
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
 
+a2 = [ones(size(a2,1), 1) a2];
 
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
 
+[val, index] = max(a3,[],2);
 
-
-
+p = index;
 
 % =========================================================================
 
