a0634e3501c37a5eabcc3082bf2015382ecaeb61
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2021-01-30 22:33:04 +0000

    [CREATE] Select Threshold

--- exercises/machine-learning-ex8/ex8/selectThreshold.m
+++ exercises/machine-learning-ex8/ex8/selectThreshold.m
@@ -23,17 +23,16 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    predictions = (pval < epsilon);
 
+    truePositives  = sum((predictions == 1) & (yval == 1));
+    falsePositives = sum((predictions == 1) & (yval == 0));
+    falseNegatives = sum((predictions == 0) & (yval == 1));
 
-
-
-
-
-
-
-
-
-
+    precision = truePositives / (truePositives + falsePositives);
+    recall = truePositives / (truePositives + falseNegatives);
+    
+    F1 = (2 * precision * recall) / (precision + recall);
 
     % =============================================================
 
